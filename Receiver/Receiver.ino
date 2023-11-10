d232696136a8a3560cc496758846e626f044e78c
nhchung11 <ngchung2002bn@gmail.com>
2023-11-10 14:12:18 +0700

    new receriver and transmitter

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,16 +1,3 @@
-
-/*
- * https://maniacbug.github.io/RF24/classRF24.html
- * 
- * VCC - 3.3v
- * GND - GND
- * CSN - 8
- * CE - 7
- * SCK - 13
- * MOSI - 11
- * MISO - 12
- */
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
@@ -29,11 +16,6 @@ void setup()
   } 
   
   radio.openReadingPipe(1, diachi);
-  //Lệnh openReadingPipe có số đường truyền từ 0-5
-  //Nhưng đường 0 đã được dùng cho ghi (mặc định)
-  //Vì vậy chỉ dùng 1-5, nếu dùng không sẽ bị chồng lấn
-  //Mở 1 kênh có địa chỉ 12345 trên đường truyền 1
-  //kênh này chỉ đọc data trên địa chỉ 12345   
   radio.setPALevel(RF24_PA_MIN);
   radio.setChannel(80);
   radio.setDataRate(RF24_250KBPS);
--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,27 +1,63 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <TinyGPS.h>
+TinyGPS gps;
 
 RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "00001";
-// Max size of this struct is 32 bytes - NRF24L01 buffer limit
-struct Data_Package {
-  int count = 0;
-};
+const byte diachi[6] = "12345"; //Mảng kí tự dạng chuỗi có 6 kí tự
 
-Data_Package data; // Create a variable with the above structure
-
-void setup() {
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+void setup()
+{
   Serial.begin(9600);
-  // ss.begin(GPSBaud);
-}   
+  if (!radio.begin()) 
+  {
+    Serial.println("Module không khởi động được...!!");
+    while (1) {}
+  } 
+  
+  radio.openWritingPipe(diachi);
+  radio.setPALevel(RF24_PA_MIN); 
+  radio.setChannel(80); 
+  radio.setDataRate(RF24_250KBPS);
+  radio.stopListening(); //Cài đặt module là TX
+  
+  if (!radio.available())
+  {
+    Serial.println("Chưa kết nối được với RX...!!");
+    Serial.println("CHỜ KẾT NỐI.......");
+  }  
+}
 
-void loop() {
-  data.count += 1;
-  radio.write(&data, sizeof(Data_Package));
-  delay(1000);
+void loop()
+{
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (Serial.available())
+    {
+      char c = Serial.read();
+      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      if (gps.encode(c)) // Did a new valid sentence come in?
+        newData = true;
+    }
+  }
+
+  if (newData)
+  {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    radio.write(&flat, sizeof(flat));
+    radio.write(&flon, sizeof(flon));
+  }
+  else
+  {
+    const char* message = "No GPS";
+    radio.write(message, strlen(message));
+    Serial.println("No GPS");
+  }
 }
